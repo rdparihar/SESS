5c3ea1f555e5d669e07874ccb57ba852865cb353
Rajdeep Singh Parihar <rdparihar@yahoo.com>
2025-08-31 22:41:14 +0530

    Fix year filter comparison in LMS view and add regression test

--- sess_dev/lms/tests.py
+++ sess_dev/lms/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .views import lmsList
+from .models import lms_details
 
-# Create your tests here.
+class LmsListViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_year_is_all_when_query_zero(self):
+        request = self.factory.get('/lms/', {'q': '0'})
+        request.session = {'emp_id': 1}
+        view = lmsList()
+        view.request = request
+        view.object_list = lms_details.objects.none()
+        context = view.get_context_data()
+        self.assertEqual(context.get('year'), 'All')
